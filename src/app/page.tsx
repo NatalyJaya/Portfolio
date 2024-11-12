08a69ed5650cbc4679efb766bb8bba97e5876a38
NatalyJaya <Nataliesalazar195@gmail.com>
2024-11-12 20:45:20 +0100

    chi :]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,89 @@
+"use client";
+
+
 import Image from "next/image";
+import instagram from './assets/img/instagram.png'
+import linkedin from './assets/img/linkedin.png'
+import github from './assets/img/github.png'
+import gmail from './assets/img/gmail.png'
+import { Typewriter } from 'react-simple-typewriter'; 
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <h1 className="text-6xl font-bold text-center sm:text-left text-[white]">
+        <Typewriter
+            words={['Hi there! I am Nat']}
+            loop={1}                 // Para repetir el efecto en bucle
+            cursor                      // Muestra el cursor de tipeo
+            cursorStyle="|"             // Define el estilo del cursor (opcional)
+            typeSpeed={70}              // Velocidad de tipeo
+            deleteSpeed={50}            // Velocidad de borrado
+            delaySpeed={1000}           // Retraso entre las palabras
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+          />
+        </h1>
+    
+
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
+        {/* <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdirs-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
+        > */}
+         
+         
+         <a
+          href="https://www.instagram.com/natvlyjs/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          <Image src={instagram} width={20} height={20} alt="Instagram icon" /> Instagram
         </a>
+         <a
+          href="https://www.instagram.com/natvlyjs/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          >
+         <Image src={linkedin} width={20}  height={20} alt="Linkedn icon"          /> Linkedin
+         
+         </a>
+         
+          <a
+            href="https://github.com/NatalyJaya"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          >
+            <Image src={github} width={20} height={20} alt="Github icon" /> Github
+          </a>         
+
+          <a
+          href="https://github.com/NatalyJaya"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center gap-2 hover:underline hover:underline-offset-4"
+          >
+            <Image src={gmail} width={20} height={20} alt="Mail icon" /> Gmail
+          </a>      
+          
+         
+        
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+         
         </a>
       </footer>
     </div>
   );
 }
+
