1da6035c9a9b772b82783a2321d3af42a273f3e5
NatalyJaya <Nataliesalazar195@gmail.com>
2024-11-13 08:56:09 +0100

    margen navbar

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -6,13 +6,13 @@ import universo from "./assets/img/universo.png"; // Use the correct path
 const Navbar = () => {
   return (
     <nav>
-      <div className="flex items-start justify-between mx-auto max-w-7xl p-4">
-        {/* Left section with the image */}
+      <div className="flex items-center justify-between mx-auto  ">
+       
         <Link href="/" className="flex items-center">
           <Image src={universo} width={40} height={40} alt="Uni icon" />
         </Link>
 
-        {/* Right section with navigation links */}
+        
         <div className="flex space-x-6 items-center">
           <Link href={'/home'} className="text-white text-lg hover:text-gray-400">
             Home
