973240d7862de3cb12d11408cb5da30307063f40
NatalyJaya <Nataliesalazar195@gmail.com>
2024-11-13 08:14:11 +0100

    home alineat

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Link from "next/link";
-import Image from "next/image"; // Importa el componente Image
-import universo from "./assets/img/universo.png"; // Usa la ruta correcta
+import Image from "next/image"; // Import the Image component
+import universo from "./assets/img/universo.png"; // Use the correct path
 
 const Navbar = () => {
   return (
     <nav>
-      <div className="flex items-center justify-between mx-auto p-4">
-       
-        <Link href="/" className="flex items-CENTER">
+      <div className="flex items-start justify-between mx-auto max-w-7xl p-4">
+        {/* Left section with the image */}
+        <Link href="/" className="flex items-center">
           <Image src={universo} width={40} height={40} alt="Uni icon" />
         </Link>
 
-        <div className="flex space-x-6">
+        {/* Right section with navigation links */}
+        <div className="flex space-x-6 items-center">
           <Link href={'/home'} className="text-white text-lg hover:text-gray-400">
             Home
           </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,27 +16,30 @@ const roboto = Roboto({
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <Navbar />
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start  justify-center">
-         
-      <h1 className="text-6xl font-bold text-center sm:text-left text-[white]">
-          <Typewriter
-            words={['Hi there! I am Nat']}
-            loop={1}                 
-            cursor
-            cursorStyle="|"             
-            typeSpeed={70}              
-            deleteSpeed={50}            
-            delaySpeed={1000}           
-          />
-        </h1>
-        <p className={`text-4xl font-bold text-center sm:text-left text-gray-300 fade-in-up`}>
-          debugging in real life
-        </p>
-        <p className="text-2xl text-center sm:text-left text-gray-100 fade-in-up">
-          I'm a computer science student with a passion for building innovative solutions.
-        </p>
+    <div className="grid grid-rows-[20px_1fr_20px]  min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <Navbar />
+      
+        <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start  justify-center">
+          
+        <h1 className="text-6xl font-bold text-center sm:text-left text-[white] ">
+            <Typewriter
+              words={['Hi there! I am Nat']}
+              loop={1}                 
+              cursor
+              cursorStyle="|"             
+              typeSpeed={70}              
+              deleteSpeed={50}            
+              delaySpeed={1000}
+                        
+            />
+          </h1>
+          <p className={`text-4xl font-bold text-center sm:text-left text-gray-300 fade-in-up `}>
+            debugging in real life
+          </p>
+          <p className="text-2xl text-center sm:text-left text-gray-100 fade-in-up">
+            I'm a computer science student with a passion for building innovative solutions.
+          </p>
+        
        
         
     
