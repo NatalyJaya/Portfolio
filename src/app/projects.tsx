9067fe2fcf1c299bb0d2a478d4265a94b6bccdf0
NatalyJaya <Nataliesalazar195@gmail.com>
2025-11-19 19:07:56 +0100

    preliminares

--- src/app/projects.tsx
+++ src/app/projects.tsx
@@ -1,6 +1,27 @@
 "use Client";
+
+
 export default function Project(){
+
+
+
     return (
-        <div> Github Projects</div>
+        <div className="mx-[10%]">
+            <div className="text-4xl font-bold text-center sm:text-left text-white "> Github Projects</div>
+            <div className="grid grid-cols-3 gap-2 my-[5%]">
+                <div className="col-span-2 border-l-blue-50 h-[550px] rounded-2xl shadow-white bg-white text-emerald-700">
+                    porject image
+                </div>
+                <div className=" place-content-center ">
+                    <div className="rounded-2xl bg-blue-300 h-[50%]">
+                        porject info
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
     );
 }
